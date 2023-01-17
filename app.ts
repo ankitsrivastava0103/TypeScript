a7831f72196f15d0cbdbce48c53eb6779e8abbd6
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-01-17 23:00:59 +0530

    Tuple Types

--- app.ts
+++ app.ts
@@ -2,10 +2,12 @@ const person: {
   name: string;
   age: number;
   hobbies: string[]
+  role: [number, string]
 } = {
   name: "Ankit",
   age: 25,
-  hobbies : ['Sports', 'Cooking']
+  hobbies : ['Sports', 'Cooking'],
+  role: [2,'author']
 };
 
 let favActivity : string[]; // Telling JS that we will be storing string array.
