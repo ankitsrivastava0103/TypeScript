a6da045ca0ca4de2174994e00a39bca0b6a6f49d
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-01-17 22:45:04 +0530

    Array Types

--- app.ts
+++ app.ts
@@ -1,9 +1,21 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[]
 } = {
   name: "Ankit",
   age: 25,
+  hobbies : ['Sports', 'Cooking']
 };
 
+let favActivity : string[]; // Telling JS that we will be storing string array.
+favActivity = ["Sports"];
+
+// let favoriteActivity: any[];
+// favoriteActivity = ['Sports', 1]
+
 console.log(person.name);
+
+for( const hobby of person.hobbies){
+  console.log(hobby.toUpperCase());
+}
