ad6dc5feb88fc48cd647ebc2ab4d3fe399916ff0
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-01-17 22:16:14 +0530

    Object Types in TS

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    // if(n1 !== 'number' || n2 !== 'number'){
-    //     throw new Error('Incorrect Input')
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhase = "Result is : ";
-add(number1, number2, printResult, resultPhase);
+var person = {
+    name: "Ankit",
+    age: 25
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if(n1 !== 'number' || n2 !== 'number'){
-  //     throw new Error('Incorrect Input')
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Ankit",
+  age: 25,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhase = "Result is : ";
-
-add(number1, number2, printResult, resultPhase);
+console.log(person.name);
