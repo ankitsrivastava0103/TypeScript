a20312d9b959d00b18487380fa81338115ebb124
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-01-18 21:02:26 +0530

    Void and Undefined Type

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-function combine(input1, input2, resultType) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultType === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    //   if(resultType === 'as-number'){
-    //     return +result
-    //   }else{
-    //     return result.toString()
-    //   }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, "as-number");
-console.log(combineAges);
-var combineStrAges = combine("30", "26", "as-number");
-console.log(combineStrAges);
-var combineNames = combine("Max", "Anna", "as-text");
-console.log(combineNames);
+function printResult(num) {
+    console.log("Result: " + num);
+}
+printResult(add(5, 12));
--- app.ts
+++ app.ts
@@ -1,33 +1,16 @@
-type Combinable = number | string;
-type ConversionDescription = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultType: ConversionDescription
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultType === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  //   if(resultType === 'as-number'){
-  //     return +result
-  //   }else{
-  //     return result.toString()
-  //   }
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 26, "as-number");
-console.log(combineAges);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combineStrAges = combine("30", "26", "as-number");
-console.log(combineStrAges);
+function print(num: number): undefined {
+  console.log("Result: " + num);
+  return;
+}
 
-const combineNames = combine("Max", "Anna", "as-text");
-console.log(combineNames);
+printResult(add(5, 12));
+
+// let someValue : undefined
