91b0d6e8a896aa7127174eb0001803e9f20e2151
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-01-18 20:05:26 +0530

    Literal Types

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultType) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultType === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
+    //   if(resultType === 'as-number'){
+    //     return +result
+    //   }else{
+    //     return result.toString()
+    //   }
     return result;
 }
-var combineAges = combine(30, 26);
+var combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
-var combineNames = combine("Max", "Anna");
+var combineStrAges = combine("30", "26", "as-number");
+console.log(combineStrAges);
+var combineNames = combine("Max", "Anna", "as-text");
 console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,15 +1,27 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultType: 'as-number' | 'as-text'
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  }else {
-    result = input1.toString() + input2.toString()
+  if (typeof input1 === "number" && typeof input2 === "number" || resultType === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
   }
+//   if(resultType === 'as-number'){
+//     return +result
+//   }else{
+//     return result.toString()
+//   }
   return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combineNames = combine("Max", "Anna");
+const combineStrAges = combine("30", "26", "as-number");
+console.log(combineStrAges)
+
+const combineNames = combine("Max", "Anna", "as-text");
 console.log(combineNames);
