3d6e9c22ad6dc3b6cc991ef8384976b5fbb12a21
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2023-01-19 19:38:03 +0530

    TS Compiling Project & Watch Mode

---
+++ union-aliases.js
@@ -0,0 +1,23 @@
+"use strict";
+function combine(input1, input2, resultType) {
+    let result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    //   if(resultType === 'as-number'){
+    //     return +result
+    //   }else{
+    //     return result.toString()
+    //   }
+    return result;
+}
+const combineAges = combine(30, 26, "as-number");
+console.log(combineAges);
+const combineStrAges = combine("30", "26", "as-number");
+console.log(combineStrAges);
+const combineNames = combine("Max", "Anna", "as-text");
+console.log(combineNames);
